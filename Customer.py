ef57e532192cdad2b5ac9db1e434051b101f0a00
LEERAY2x <agudelonataliejo@gmail.com>
2026-09-06 18:53:02 +0800

    Update Customer.py
    w3qserdftyghunji

--- Customer.py
+++ Customer.py
@@ -6,8 +6,8 @@ import random
 # 4.) Edit user detaile
 # 5.) deleter user
 
-class Customer:
-    def __init__(self, unique_ID, name, address, phone):       # define the class and attribute
+class Customer:                                      # define the class and attribute
+    def __init__(self, unique_ID, name, address, phone):      
         self.unique_ID = unique_ID
         self.name = name
         self.address = address
@@ -37,92 +37,80 @@ def newCustomer():                                             # create new user
     customerList.append(customer)
     print("Customer added!")
 
-def manageCustomer():
+
+def deleteCustomer():                                          # deletes exisitng user
+    findID = int(input("Enter the ID of the user you would like to remove: "))  
+     
+    for Customer in customerList:  
+        if Customer.unique_ID == findID:                  
+            print("Unique ID:", Customer.unique_ID) 
+            print("Name:", Customer.name) 
+            print("Address:", Customer.address)                      
+            print("Phone Number:", Customer.phone) 
+            print()  
+ 
+            while True:  
+                confirm = input(
+                    "Are you sure to permanently delete this user? (y/n): "
+                ) 
+
+                if confirm.lower() == "y": 
+                    customerList.remove(Customer) 
+                    print("User deleted successfully!")
+                    return
+
+                elif confirm.lower() == "n": 
+                    return
+
+                else: 
+                    print("Invalid. Please enter y or n.")
+
+    else: 
+        print("That ID does not exist")
+
+def editInfo():                                      # edits existing user info
     findID = int(input("Enter the ID of the user you would like to edit: "))
 
     for Customer in customerList:
         if Customer.unique_ID == findID:
-            print("Unique ID:", Customer.unique_ID)                 # input the id you want to manage
-            print("Name:", Customer.name)                           # shows the ID information
-            print("Adress:", Customer.address )
+
+            print("Unique ID:", Customer.unique_ID)
+            print("Name:", Customer.name)
+            print("Address:", Customer.address)
             print("Phone Number:", Customer.phone)
             print()
 
-def manageCustomer(): 
-    findID = int(input("Enter the ID of the user you would like to edit: ")) 
-
-    for Customer in customerList: 
-        if Customer.unique_ID == findID: 
-            
-            print("Unique ID:", Customer.unique_ID)
-            print("Name:", Customer.name)
-            print("Address:", Customer.address)                     # shows User Info
-            print("Phone Number:", Customer.phone)
-            print() 
-
-            while True:  
-                option = int(input(
-                    "Would you like to:\n"
-                    "edit   [1]\n"                                  # options to manage Info
-                    "delete [2]\n"
-                    "return [3]\n"
-                    "Enter Option: "
+            while True:
+                choice = int(input(
+                    "What would you like to edit?\n"
+                    "name         [1]\n"
+                    "address      [2]\n"
+                    "phone number [3]\n"
+                    "none         [4]\n"
+                    "option: "
                 ))
-                print() 
 
-                if option == 1: 
-                    while True: 
-                        choice = int(input(
-                            "What would you like to edit?\n"
-                            "name         [1]\n"
-                            "address      [2]\n"                # edit info
-                            "phone number [3]\n"
-                            "none         [4]\n"
-                            "option: "
-                        ))
+                if choice == 1:
+                    Customer.name = input("Enter the new name: ")
 
-                        if choice == 1:                                             
-                            Customer.name = input("Enter the new name: ") 
+                elif choice == 2:
+                    Customer.address = input("Enter new address: ")
 
-                        elif choice == 2: 
-                            Customer.address = input("Enter new address: ") 
+                elif choice == 3:
+                    Customer.phone = input("Enter new phone number: ")
 
-                        elif choice == 3: 
-                            Customer.phone = input("Enter new phone number: ") 
-
-                        elif choice == 4: 
-                            break
-
-                        else:
-                            print("Invalid Option.")
-
-                elif option == 2:                                       # delete User
-                    while True: 
-                        confirm = input(
-                            f"Are you sure you are removing "
-                            f"{Customer.name} permanently? (y/n): "     # confirmation
-                        )
-
-                        if confirm.lower() == "y":                                  
-                            customerList.remove(Customer) 
-                            print("Customer deleted successfully!")
-                            return
-
-                        elif confirm.lower() == "n": 
-                            return
-
-                        else: 
-                            print("Invalid Option, please enter y or n.")
-
-                elif option == 3:                                           
+                elif choice == 4:
                     return
 
-                else: 
-                    print("Invalid Option!")
+                else:
+                    print("Invalid Option.")
 
             break
 
-    else:  
-        print("That ID does not exist")
+    else:
+        print("That ID does not exist.")
+
+
+
 
 
